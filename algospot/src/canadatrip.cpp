e61f28f8ccd953715e8df6ab7bc243f082b2b4f8
al7ech <al7ech@gmail.com>
2019-03-21 11:08:14 +0900

    canadatrip WA

---
+++ algospot/src/canadatrip.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <string.h>
+#include <iostream>
+#include <math.h>
+#include <queue>
+using namespace std;
+typedef long long ll;
+
+typedef struct City
+{
+    int L,M,G;
+}city;
+
+int T;
+int N,K;
+vector<city> D;
+
+int count(int x)
+{
+    int ret = 0;
+    for(city C: D)
+    {
+        ret += max(0,min(C.M/C.G,(x-(C.L-C.M))/C.G) + 1);
+    }
+    return ret;
+}
+
+int main()
+{
+    scanf("%d",&T);
+
+    for(int t=0;t<T;t++)
+    {
+        scanf("%d %d",&N,&K);
+        D.clear();
+        int max_x = -1;
+        for(int i=0;i<N;i++)
+        {
+            city X;
+            scanf("%d %d %d",&X.L,&X.M,&X.G);
+            D.push_back(X);
+            max_x = max(max_x,X.L);
+        }
+        int min_x = 0;
+
+        while(min_x < max_x)
+        {
+            int mid_x = (min_x + max_x) / 2;
+            if(count(mid_x) >= K) max_x = mid_x;
+            else min_x = mid_x + 1;
+        }
+
+        printf("%d\n",min_x);
+
+    }
+    return 0;
+}
