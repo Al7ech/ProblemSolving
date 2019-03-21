f64ac726a9d1ef8002f5dc89946b2f15514dd9d8
al7ech <al7ech@gmail.com>
2019-03-21 13:54:04 +0900

    1300 WA

--- acmicpc/src/1300.cpp
+++ acmicpc/src/1300.cpp
@@ -12,15 +12,18 @@ typedef long long ll;
 
 int N,K;
 
-int D[100005];
+ll D[100005];
+int cnt;
 
 ll count(int n,ll val)    //  return number of values not larger than val in n*n square
 {
-    int &ret = D[n];
+    //printf("%d %d\n",n,cnt);
+    ll &ret = D[n];
     if(ret) return ret;
+    cnt++;
     if(n == 1) return D[n] = (val>=1);
 
-    return ret = count(n-1,val) + (val/n - (val > n*n));
+    return ret = count(n-1,val) + (min((ll)2*n,2*(val/n)) - (val >= n*n));
 }
 
 int main()
@@ -35,8 +38,16 @@ int main()
         memset(D,0,sizeof(D));
         ll mid = (min+max)/2;
 
-        if(count(N,mid) > K) 
+        //rintf("%lld %lld %lld %lld\n",min,mid,max,count(N,mid));
+        //for(int i=1;i<=N;i++) printf("%d\n",D[i]);
+
+        if(count(N,mid) >= (ll)K)
+            max = mid;
+        else
+            min = mid+1;
     }
+
+    printf("%lld\n",min);
 
     return 0;
 }
