95895194700d8b4b51203eb086fb0c21fda115df
al7ech <al7ech@gmail.com>
2019-04-14 22:42:17 +0900

    added header template

---
+++ headers.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <string.h>
+#include <iostream>
+#include <math.h>
+#include <queue>
+using namespace std;
+typedef long long ll;
