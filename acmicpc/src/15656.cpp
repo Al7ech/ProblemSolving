f3b706f4476f9b02058d9b5f357034249571f33e
al7ech <al7ech@gmail.com>
2019-07-19 09:47:36 +0900

    15656 AC

---
+++ acmicpc/src/15656.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N,M;
+int value[10];
+vector<int> V;
+
+void dfs(int idx,int depth)
+{
+    V.push_back(idx);
+
+    if(depth == M)
+    {
+        for(int i : V)
+            printf("%d ",value[i-1]);
+        puts("");
+    }
+    else
+    {
+        for(int i=1;i<=N;i++)
+            dfs(i,depth+1);
+    }
+
+    V.pop_back();
+}
+
+int main(void)
+{
+    scanf("%d %d",&N,&M);
+    for(int i=0;i<N;i++)
+        scanf("%d",value+i);
+    sort(value,value+N);
+
+    for(int i=1;i<=N;i++)
+        dfs(i,1);
+
+    return 0;
+}
