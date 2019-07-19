9453a1c153dae2d51c87cf73e69568de8070ca18
al7ech <al7ech@gmail.com>
2019-07-19 09:38:42 +0900

    15651 AC

---
+++ acmicpc/src/15651.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+int N,M;
+vector<int> V;
+
+void dfs(int idx,int depth)
+{
+    V.push_back(idx);
+
+    if(depth == M)
+    {
+        for(int i : V)
+            printf("%d ",i);
+        puts("");
+    }
+    else
+    {
+        for(int i=1;i<=N;i++)
+            dfs(i,depth+1);
+    }
+
+    V.pop_back();
+}
+
+int main(void)
+{
+    scanf("%d %d",&N,&M);
+
+    for(int i=1;i<=N;i++)
+        dfs(i,1);
+
+    return 0;
+}
