1f3224e5c6af02ace209d26f9e4849927528076e
Dongryul Lee <al7ech@gmail.com>
2019-07-25 10:32:33 +0900

    15665 AC

---
+++ acmicpc/src/15665.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N,M;
+int value[10];
+int cnt[20000];
+vector<int> V;
+
+void dfs(int idx,int depth)
+{
+    V.push_back(idx);
+    cnt[value[idx]]--;
+
+    if(depth == M)
+    {
+        for(int i:V)
+        {
+            printf("%d ",value[i]);
+        }
+        puts("");
+    }
+    else
+    {
+        int prev = -1;
+
+        for(int i=0;i<N;i++)
+        {
+            if(prev != value[i])
+            {
+                dfs(i,depth+1);
+                prev = value[i];
+            }
+        }   
+    }
+
+    V.pop_back();
+    cnt[value[idx]]++;
+}
+
+int main(void)
+{
+    scanf("%d %d",&N,&M);
+    for(int i=0;i<N;i++)
+    {
+        scanf("%d",value+i);
+        cnt[value[i]]++;
+    }
+    sort(value,value+N);
+
+    vector<int> visit(10005,0);
+
+    for(int i=0;i<N;i++)
+    {
+        if(!visit[value[i]])
+        {
+            visit[value[i]] = 1;
+            dfs(i,1);
+        }
+    }
+
+    return 0;
+}
