ae448dee69a7050b2604499119dcbdf5a95c6b59
Dongryul Lee <al7ech@gmail.com>
2019-07-31 14:06:58 +0900

    2014 WA

--- acmicpc/src/2014.cpp
+++ acmicpc/src/2014.cpp
@@ -1,31 +1,29 @@
 #include <stdio.h>
-#include <queue>
+#include <set>
 
 using namespace std;
 typedef long long ll;
 
 int K, N;
 int A[100];
-priority_queue<ll> pq;
-
+set<int> s;
 int main(void)
 {
     scanf("%d%d", &K, &N);
     for (int i = 0; i < K; i++)
     {
         scanf("%d", A + i);
-        pq.push(-(ll)A[i]);
+        s.insert((ll)A[i]);
     }
     ll t;
     for (int i = 0; i < N; i++)
     {
-        t = -pq.top();
-        pq.pop();
-        while (-t == pq.top())
-            pq.pop();
+        t = *s.begin();
+        s.erase(t);
 
         for (int j = 0; j < K; j++)
-            pq.push(-t * A[j]);
+            if (t * A[j] < (1 << 31))
+                s.insert(t * A[j]);
     }
     printf("%lld\n", t);
 
